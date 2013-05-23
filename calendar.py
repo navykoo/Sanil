d2ccf8f05fbbfbb90506ad36549f3aec29ce68a8
navy <ghjsea@126.com>
2013-05-23 22:13:33 +0800

    add calendar (parially)

--- calendar.py
+++ calendar.py
@@ -1,8 +1,10 @@
+#-*-coding:utf-8-*-
+import time
+import tkFont
+from Tkinter import *
 class Calendar:
     pass
 AppCal = Calendar()
-import time
-import tkFont
 
 def calcFirstDayOfMonth(year,month,day):
     months = (0,31,59,90,120,151,181,212,243,273,304,334)
@@ -28,7 +30,7 @@ def createMonth(master):
     for i in range(6):
         for j in range(7):
             Label(master,text = '').grid(row = i + 2,column = j)
-def updateDate():
+def updateDate(self):
     ft1 = tkFont.Font(weight=tkFont.BOLD)
     ft2 = tkFont.Font()
     #.........
@@ -43,17 +45,17 @@ def updateDate():
     for i in range(6):
 	#print i
         for j in range(7):
-            root.grid_slaves(i +2,j)[0]['text'] = ''
+            self.grid_slaves(i +2,j)[0]['text'] = ''
 
     #print fd
     for i in range(1,months[month - 1] + 1):
 	#print i
 	if (i == day):
-	        root.grid_slaves((i + fd - 1)/7 + 2,(i + fd -1)%7)[0]['text'] = str(i)
-	        root.grid_slaves((i + fd - 1)/7 + 2,(i + fd -1)%7)[0]['font'] = ft1
+	        self.grid_slaves((i + fd - 1)/7 + 2,(i + fd -1)%7)[0]['text'] = str(i)
+	        self.grid_slaves((i + fd - 1)/7 + 2,(i + fd -1)%7)[0]['font'] = ft1
 	else:
-	        root.grid_slaves((i + fd - 1)/7 + 2,(i + fd -1)%7)[0]['text'] = str(i)
-	        root.grid_slaves((i + fd - 1)/7 + 2,(i + fd -1)%7)[0]['font'] = ft2
+	        self.grid_slaves((i + fd - 1)/7 + 2,(i + fd -1)%7)[0]['text'] = str(i)
+	        self.grid_slaves((i + fd - 1)/7 + 2,(i + fd -1)%7)[0]['font'] = ft2
     
 def drawHeader(master):
     # ..............
@@ -63,39 +65,44 @@ def drawHeader(master):
     # ......
     AppCal.vYear = StringVar()
     AppCal.vYear.set(now[0])
-    Label(master,text = 'YEAR').grid(row = 0,column = col_idx);col_idx += 1
+    Label(master,text = '年').grid(row = 0,column = col_idx);col_idx += 1
     omYear = apply(OptionMenu,(master,AppCal.vYear) + tuple(range(2010,2020)))
     omYear.grid(row = 0,column = col_idx);col_idx += 1
 
     # ......
     AppCal.vMonth = StringVar()
     AppCal.vMonth.set(now[1])
-    Label(master,text = 'Month').grid(row = 0,column = col_idx);col_idx += 1
+    Label(master,text = '月').grid(row = 0,column = col_idx);col_idx += 1
     omMonth = apply(OptionMenu,(master,AppCal.vMonth) + tuple(range(1,12)))
     omMonth.grid(row = 0,column = col_idx);col_idx += 1
 
     # ......
     AppCal.vDay = StringVar()
     AppCal.vDay.set(now[2])
-    Label(master,text = 'DAY').grid(row = 0,column = col_idx);col_idx += 1
+    Label(master,text = '日').grid(row = 0,column = col_idx);col_idx += 1
     omDay = apply(OptionMenu,(master,AppCal.vDay) + tuple(range(1,32)))
     omDay.grid(row = 0,column = col_idx);col_idx += 1
 
     # ......
-    btUpdate = Button(master,text = 'Update',command = updateDate)
-    btUpdate.grid(row = 0,column = col_idx);col_idx += 1
+    #btUpdate = Button(master,text = '更新',command = updateCalendar)
+    #btUpdate.grid(row = 0,column = col_idx);
+		#col_idx += 1
 
     # ......
-    weeks = ['Sun.','Mon.','Tues.','Wed.','Thurs.','Fri.','Sat.']
+    weeks = ['日','一','二','三','四','五','六']
     for week in weeks:
         Label(master,text = week).grid(row = 1,column = weeks.index(week))
 
+def displayCalendar(root):
+		drawHeader(root)
+		createMonth(root)
+		updateDate(root)
     
-from Tkinter import *
-root = Tk()
+#from Tkinter import *
+#root = Tk()
+#display(root)
+#drawHeader(root)
+#createMonth(root)
+#updateDate()
 
-drawHeader(root)
-createMonth(root)
-updateDate()
-
-root.mainloop()
+#root.mainloop()
